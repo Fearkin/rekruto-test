3fbd0c74ad16a19b60656ba1e0cf65fa4ee80d0b
Fearkin <belyaev38@inbox.ru>
2022-03-16 16:11:29 +0300

    added index view and template

--- hello_service/views.py
+++ hello_service/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    if request.method == "GET":
+        name = request.GET.get("name", "")
+        message = request.GET.get("message", "")
+        context = {"name": name, "message": message}
+        return render(request, "hello_service/index.html", context)
+    return HttpResponse("Should be GET request")
